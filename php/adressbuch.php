1e66c5e80413c1c002da8e955aa05fa43e4eb81d
Miruku <archangel0660@gmail.com>
2018-12-07 09:18:13 +0100

    Register/Login/logout prozess

--- php/adressbuch.php
+++ php/adressbuch.php
@@ -1,21 +1,36 @@
 <?php
     require 'dataBaseConnection.php';
     session_start();
-    
-    header('Location: ../html/adressbuch.html');
-
-    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
-    {
-        $massage = "Erfolgreich eingeloggt!";
-        echo "<script type='text/javascript'>alert('$massage');</script>";
-        header('Location: ../html/adressbuch.html');
-    } 
-    else 
-    {
-        $error_massage = "Einloggen fehlgeschlagen, session wurde nicht gefunden!";
-        echo "<script type='text/javascript'>alert('$error_massage');</script>";
-        header('Location: login.php');
-    }
 ?>
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Adressbuch Pro</title>
+  </head>
+  <body>
+    <h1>Adressbuch Pro</h1>
+    <?php
+        if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
+            echo '<a href="logout.php" class="badge badge-primary">Logout</a>';
+        else
+        {
+           echo '<a href="login.php" class="badge badge-primary">Login</a>';
+           echo '<a href="register.php" class="badge badge-primary">Registrieren</a>';
+        }
+    ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
 
